c0159e25daaaac14691dd025ad0e2904e9302428
Krispy King <kking620@gmail.com>
2025-11-13 15:13:50 -0800

    Second commit

---
+++ 210-lab-32.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <deque>
+#include <cstdlib>
+#include <cmath>
+#include "Car.h"
+
+using namespace std;
+
+const int INITIAL_SIZE = 2;
+
+int main() {
+    srand(time(0));
+    
+    deque<Car> tollBooth;
+
+    
+}
